baa352350e2da5d2570a787683c403e96daec61c
Carlos Bruno <cbcarlos08@gmail.com>
2017-05-14 15:24:55 -0400

    Curso de Android

--- app/src/main/java/sharedpreferences/cursoandroid/com/sharedpreferences/MainActivity.java
+++ app/src/main/java/sharedpreferences/cursoandroid/com/sharedpreferences/MainActivity.java
@@ -1,13 +1,56 @@
 package sharedpreferences.cursoandroid.com.sharedpreferences;
 
+import android.app.Activity;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    private EditText textoNome;
+    private Button   botaoSalvar;
+    private TextView textoExibicaot;
+
+    private static final String ARQUIVO_PREFERENCIA = "Arquivo preferencia";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textoNome = (EditText) findViewById(R.id.textoNomeId);
+        textoExibicaot = (TextView) findViewById(R.id.textoExibicaoId);
+        botaoSalvar = (Button) findViewById(R.id.botaoSalvarId);
+
+        botaoSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences sharedPreferences = getSharedPreferences(ARQUIVO_PREFERENCIA, 0); //0 ARQUIVO PRIVADO
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+
+                if( textoNome.getText().toString().equals("") ){
+                    Toast.makeText(MainActivity.this, "Por favor preencher o nome", Toast.LENGTH_SHORT).show();
+
+                }else{
+                    editor.putString("nome", textoNome.getText().toString());
+                    editor.commit();
+                    textoExibicaot.setText("Ola, "+textoNome.getText().toString());
+                }
+            }
+        });
+
+        //recupera os dados salvos
+        SharedPreferences sharedPreferences = getSharedPreferences(ARQUIVO_PREFERENCIA, 0); //0 ARQUIVO PRIVADO
+        if( sharedPreferences.contains("nome")){
+            String nomeUsuario = sharedPreferences.getString("nome", "usuário não definido");
+            textoExibicaot.setText("Olá, " + nomeUsuario);
+        }else{
+            textoExibicaot.setText("Olá, usuário não definido");
+        }
     }
 }
